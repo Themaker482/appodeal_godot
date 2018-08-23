7308a1cb4a5b336abad803e9961c5b3e77349716
themaker482 <carmonamendozabernabe@gmail.com>
2018-08-23 06:41:21 -0500

    Update config.py

--- appodeal/config.py
+++ appodeal/config.py
@@ -6,5 +6,6 @@ def configure(env):
 		env.android_add_java_dir("android/src")
 		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
 		env.android_add_dependency("compile 'com.google.android.gms:play-services-ads:10.2.1'")
+		env.android_add_dependency("compile 'com.google.android.gms:play-services-location:10.2.1'")
 		env.android_add_dependency("compile fileTree(dir: '../../../modules/appodeal/android/libs', include: '*.jar')")
 		
